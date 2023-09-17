ab85b155b1d1a1d35a9664fc2678b48d13e8ca40
petuch03 <egormsafronov@gmail.com>
2023-09-17 13:11:24 +0300

    numbered list enhancement: paste into indentation list

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import {Editor, EditorPosition} from "obsidian";
 
-const numberedIndexMatcher  = /^(\s*)(\d+)\./
+const numberedIndexMatcher = /^(\s*)(\d+)\./;
 
 export function adjustNumberedContent(content: string, editor: Editor): string {
 	const doc: Editor = editor.getDoc();
@@ -18,34 +18,39 @@ export function adjustNumberedContent(content: string, editor: Editor): string {
 		lastNumber = parseInt(currentLineMatch[2]);
 		isPastingAfterNumber = true;
 	} else {
-		for (let i = linesBeforeCursor.length - 1; i >= 0; i--) {
-			const match: RegExpMatchArray | null = linesBeforeCursor[i].match(numberedIndexMatcher);
-			if (match) {
-				lastIndentation = match[1];
-				lastNumber = parseInt(match[2]);
-				break;
+		// Grab the line just before the current one
+		const previousLine: string = linesBeforeCursor[linesBeforeCursor.length - 2] || "";
+		const previousLineMatch: RegExpMatchArray | null = previousLine.match(numberedIndexMatcher);
+
+		if (previousLineMatch) {
+			lastIndentation = previousLineMatch[1];
+			lastNumber = parseInt(previousLineMatch[2]);
+		} else {
+			for (let i = linesBeforeCursor.length - 1; i >= 0; i--) {
+				const match: RegExpMatchArray | null = linesBeforeCursor[i].match(numberedIndexMatcher);
+				if (match) {
+					lastIndentation = match[1];
+					lastNumber = parseInt(match[2]);
+					break;
+				}
 			}
 		}
 	}
+	console.log(`lastNumber ${lastNumber}`);
+	console.log(`lastIndentation '${lastIndentation}'`);
 
 	if (lastNumber !== null) {
-		const linesFromClipboard : string[] = content.split('\n');
+		const linesFromClipboard: string[] = content.split('\n');
 		const adjustedLines = [];
 
 		for (let i = 0; i < linesFromClipboard.length; i++) {
 			const line = linesFromClipboard[i];
-			const currentIndentationMatch = line.match(/^\s*/);
-			const currentIndentation = currentIndentationMatch ? currentIndentationMatch[0] : '';
 
-			if (currentIndentation === lastIndentation) {
-				if (isPastingAfterNumber && i === 0) {
-					adjustedLines.push(`${line.trim().replace(/^\d+\.\s*/, '')}`);
-				} else {
-					lastNumber++;
-					adjustedLines.push(`${lastIndentation}${lastNumber}. ${line.trim().replace(/^\d+\.\s*/, '')}`);
-				}
+			if (isPastingAfterNumber && i === 0) {
+				adjustedLines.push(`${line.trim().replace(/^\d+\.\s*/, '')}`);
 			} else {
-				adjustedLines.push(line);
+				lastNumber++;
+				adjustedLines.push(`${lastIndentation}${lastNumber}. ${line.trim().replace(/^\d+\.\s*/, '')}`);
 			}
 		}
 
