07549b2381196abd7f252e37e38f24eb703d8fa9
petuch03 <egormsafronov@gmail.com>
2023-09-17 13:12:30 +0300

    numbered list enhancement: optimisation with taking only previous and current lines

--- src/utils.ts
+++ src/utils.ts
@@ -5,8 +5,8 @@ const numberedIndexMatcher = /^(\s*)(\d+)\./;
 export function adjustNumberedContent(content: string, editor: Editor): string {
 	const doc: Editor = editor.getDoc();
 	const cursorPosition: EditorPosition = doc.getCursor();
-	const linesBeforeCursor: string[] = doc.getRange({line: 0, ch: 0}, cursorPosition).split('\n');
-	const currentLine: string = linesBeforeCursor[linesBeforeCursor.length - 1];
+	const currentLine: string = doc.getLine(cursorPosition.line) || "";
+	const previousLine: string = cursorPosition.line > 0 ? doc.getLine(cursorPosition.line - 1) : "";
 
 	let lastNumber = null;
 	let lastIndentation = '';
@@ -18,24 +18,13 @@ export function adjustNumberedContent(content: string, editor: Editor): string {
 		lastNumber = parseInt(currentLineMatch[2]);
 		isPastingAfterNumber = true;
 	} else {
-		// Grab the line just before the current one
-		const previousLine: string = linesBeforeCursor[linesBeforeCursor.length - 2] || "";
 		const previousLineMatch: RegExpMatchArray | null = previousLine.match(numberedIndexMatcher);
-
 		if (previousLineMatch) {
 			lastIndentation = previousLineMatch[1];
 			lastNumber = parseInt(previousLineMatch[2]);
-		} else {
-			for (let i = linesBeforeCursor.length - 1; i >= 0; i--) {
-				const match: RegExpMatchArray | null = linesBeforeCursor[i].match(numberedIndexMatcher);
-				if (match) {
-					lastIndentation = match[1];
-					lastNumber = parseInt(match[2]);
-					break;
-				}
-			}
 		}
 	}
+
 	console.log(`lastNumber ${lastNumber}`);
 	console.log(`lastIndentation '${lastIndentation}'`);
 
